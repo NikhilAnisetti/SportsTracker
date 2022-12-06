1d4bd3af19e88509dfa8621dc49d7e22881ef78a
NikhilAnisetti <114839124+NikhilAnisetti@users.noreply.github.com>
2022-12-06 00:11:50 +0000

    43 minutes in TIM

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def home():
-    return {"Data": "home"}
+class Game(BaseModel):
+    LA: int
+    Boston: int
+    BestPlayer: str
+
+
+scores = {
+}
+
+
+@app.get("/get-score/{game}/{my_team}")
+def get_item(game: int, my_team=Path(None, description="Name of your city")):
+    return getattr(scores[game], my_team)
+
+
+@app.get("/get-highest-score")
+def get_item(player: str):
+    NumberOfTimesBestPlayer = 0
+    for game in scores:
+        if getattr(scores[game], "BestPlayer") == player:
+            NumberOfTimesBestPlayer += 1
+
+    return NumberOfTimesBestPlayer
+
+
+@app.post("/create-game/{game_id}")
+def create_game(game_id: int, game: Game):
+    if game_id in scores:
+        return {"Game already exists"}
+
+    scores[game_id] = game
+    return scores[game_id]
